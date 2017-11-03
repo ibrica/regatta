5ae0b136f7dbed609af6a104674386ad18f98e9e
ibritvic <ivbritvic@gmail.com>
2017-11-03 15:56:22 +0100

    Test added

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import regattaApp from '../reducers'
+
+let store = createStore(regattaApp)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(    <Provider store={store}>
+                        <App />
+                      </Provider>, div);
 });
